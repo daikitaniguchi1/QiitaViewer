d6f62fca191879a7ef44059116d68be7266571a1
daikitaniguchi1 <t.daiki50@gmail.com>
2014-07-15 15:35:15 +0900

    tweak webview

--- app/controllers/entry_controller.rb
+++ app/controllers/entry_controller.rb
@@ -4,9 +4,19 @@ class EntryController < UIViewController
   def viewDidLoad
     super
     
-    webview = UIWebView.new
-    webview.frame = view.frame # webviewの表示サイズを調整
-    view.addSubview(webview)
+    # webview = UIWebView.new
+    # webview.frame = view.frame # webviewの表示サイズを調整
+    # view.addSubview(webview)
+    # webview.loadHTMLString(@entry.body, baseURL: nil)
+
+    webview = UIWebView.new.tap do |v|
+      v.frame = self.view.bounds
+      v.scalesPageToFit = true
+      # v.loadRequest(NSURLRequest.requestWithURL(NSURL.URLWithString(self.item[:link])))
+      v.delegate = self
+      view.addSubview(v)
+    end
     webview.loadHTMLString(@entry.body, baseURL: nil)
+
   end
 end

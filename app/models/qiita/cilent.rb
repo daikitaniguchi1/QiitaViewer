395b9062e5e7d973cc1f24a0530d97b6ee781fd0
daikitaniguchi1 <t.daiki50@gmail.com>
2014-07-15 15:49:49 +0900

    change request api to /items for selecting hot entries.

--- app/models/qiita/cilent.rb
+++ app/models/qiita/cilent.rb
@@ -10,9 +10,12 @@ module Qiita
   #  }
   class Client
     BASE_URL = 'https://qiita.com/api/v1'
+    PER_PAGE = 100
+    THRESHOLD = 10
 
     def self.fetch_tagged_items(tag_name, &block)
-      url = BASE_URL + "/tags/#{tag_name}/items"
+      url = BASE_URL + "/items.json?per_page=#{PER_PAGE}"
+      # url = BASE_URL + "/tags/#{tag_name}/items"
       BW::HTTP.get(url) do |response|
         items = []
         message = nil
@@ -20,7 +23,17 @@ module Qiita
           if response.ok?
             json = BW::JSON.parse(response.body.to_s)
             # 1件ずつQiita::Itemクラスのインスタンスにして格納
-            items = json.map {|data| Qiita::Item.new(data)}
+            # per_page=100の場合は、だいたい直近20時間の間に何ストックされたか。
+            items = json.map.with_index(1) {|data, i|
+              next if data['stock_count'].to_i < THRESHOLD
+                p "========================"
+                p "index: #{i}"
+                p "title: #{data['title']}"
+                p "stock: #{data['stock_count']}"
+                p "========================"
+              Qiita::Item.new(data)
+            }.compact!
+
           else
             if response.body.nil?
               message = response.error_message

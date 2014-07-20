1cbb2818dab4f4251e1a36da9c00010de198fc5c
daikitaniguchi1 <t.daiki50@gmail.com>
2014-07-20 23:18:50 +0900

    tweak EntryCell

--- app/views/entry_cell.rb
+++ app/views/entry_cell.rb
@@ -10,5 +10,18 @@ class EntryCell < UITableViewCell
     super
     self.textLabel.text = @entry.title
     self.detailTextLabel.text = "#{@entry.stocks} stocks! by #{@entry.username}"
+    # p @entry.image
+    # url = NSURL.URLWithString(@entry.image)
+    # data = NSData.dataWithContentsOfURL(url)
+    # image = UIImage.imageWithData(data)
+    # @photo = UIImageView.alloc.initWithFrame(CGRectMake(10.0, 10.0, 200.0, 100.0))
+    # @photo.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleHeight
+    # @photo.image = image
+    # @name_label = UILabel.alloc.init
+    # @name_label.textColor = UIColor.colorWithRed(0, green:0, blue:0, alpha:1.000)
+    # @name_label.text = @entry.title
+    # self.addSubview(@name_label)
+    # self.addSubview(@photo)
+    # self
   end
 end

34eb236acb8ef9470ad7bf8c76efae2f92992f6b
daikitaniguchi1 <t.daiki50@gmail.com>
2014-08-08 22:56:05 +0900

    navbar color

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,8 +1,20 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    self.configure_navigation_bar
+
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = UINavigationController.alloc.initWithRootViewController(EntriesController.new)
     @window.makeKeyAndVisible
+
     true
   end
+
+  def configure_navigation_bar
+    UINavigationBar.appearance.barTintColor = UIColor.colorWithRed(0.349, green:0.733, blue:0.047, alpha:0.7)
+    UINavigationBar.appearance.titleTextAttributes = {
+        NSForegroundColorAttributeName => UIColor.whiteColor,
+        NSFontAttributeName            => UIFont.boldSystemFontOfSize(18)
+    }
+  end
+
 end

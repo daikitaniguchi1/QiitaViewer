babed03a0500fe26db6b3e30aedf048f561e3b55
daikitaniguchi1 <t.daiki50@gmail.com>
2014-08-09 21:42:00 +0900

    tweak navbar color

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -10,7 +10,7 @@ class AppDelegate
   end
 
   def configure_navigation_bar
-    UINavigationBar.appearance.barTintColor = UIColor.colorWithRed(0.349, green:0.733, blue:0.047, alpha:0.7)
+    UINavigationBar.appearance.barTintColor = UIColor.colorWithRed(0.249, green:0.833, blue:0.047, alpha:0.7)
     UINavigationBar.appearance.titleTextAttributes = {
         NSForegroundColorAttributeName => UIColor.whiteColor,
         NSFontAttributeName            => UIFont.boldSystemFontOfSize(18)
